edcd653dc807bb1eac1c1eee47016d8de34e325a
LEEJAEHYUB <woguqdl20@gmail.com>
2020-03-10 23:12:47 +0900

    Complete_Project_Page

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -3,6 +3,9 @@ import React from "react";
 import { Row, Col } from "antd";
 import "./Project.css";
 import { FiCalendar } from "react-icons/fi";
+import { TiSpannerOutline } from "react-icons/ti";
+import { GoNote } from "react-icons/go";
+import { FaGithub, FaAndroid } from "react-icons/fa";
 
 import finalPic from "../images/finalProject.gif";
 import finalPic3 from "../images/finalProject3.gif";
@@ -14,7 +17,7 @@ function Project() {
 			<Row gutter={[8, 16]}>
 				<Col span={4} />
 				<Col span={16}>
-					<div style={{ fontSize: "40px", textAlign: "center" }}>PROJECTs</div>
+					<div style={{ fontSize: "50px", textAlign: "center" }}>PROJECTs</div>
 				</Col>
 				<Col span={4} />
 			</Row>
@@ -27,18 +30,45 @@ function Project() {
 							<img className="imgBox-1" src={finalPic} alt="finalProject1" onClick={() => {}} />
 							<img className="imgBox-1" src={finalPic3} alt="finalProject2" />
 						</Col>
-						<Col flex="auto" className="contentRight">
+						<Col flex="auto" className="contentRight" style={{ padding: 0 }}>
 							<div>
-								<span style={{ fontSize: "30px", fontWeight: "bold" }}>
+								<span style={{ fontSize: "45px", fontWeight: "bold" }}>
 									Serendipity - eat, pray, love
 								</span>
 
-								<span style={{ fontSize: "20px", fontWeight: "bold", marginLeft: "3%" }}>
+								<span style={{ fontSize: "30px", fontWeight: "bold", marginLeft: "3%" }}>
 									Team Serengeti - Team Member
 								</span>
 							</div>
 							<div style={{ marginTop: 10 }}>
-								<FiCalendar size={15} /> <span style={{ fontSize: 16 }}>01/2019 ~ 02/2019</span>
+								<FiCalendar size={20} /> <span style={{ fontSize: 20 }}>01/2019 ~ 02/2019</span>
+							</div>
+							<div style={{ marginLeft: -2, marginTop: 10 }}>
+								<TiSpannerOutline size={23} /> <span style={{ fontSize: 20 }}>Front-End</span>
+							</div>
+							<div style={{ marginTop: 10 }}>
+								<GoNote size={23} /> <span style={{ fontSize: 20 }}>직장인을 위한 만남 주선 어플</span>
+							</div>
+							<div style={{ marginTop: 10 }}>
+								<FaGithub size={23} />
+								<span
+									className="webSite"
+									style={{ fontSize: 20 }}
+									onClick={() => {
+										window.open("https://github.com/LEEJAEHYUB/Serendipity_client");
+									}}
+								>
+									https://github.com/LEEJAEHYUB/Serendipity_client
+								</span>
+							</div>
+							<div
+								style={{ marginTop: 10 }}
+								className="webSite"
+								onClick={() => {
+									window.open("https://tuney.kr/Av6qFX");
+								}}
+							>
+								<FaAndroid size={23} /> <span style={{ fontSize: 20 }}>https://tuney.kr/Av6qFX</span>
 							</div>
 						</Col>
 					</Row>
@@ -54,16 +84,34 @@ function Project() {
 							<img className="imgBox" src={firstPic} alt="finalProject" />
 						</Col>
 
-						<Col flex="auto" className="contentRight">
+						<Col flex="auto" className="contentRight" style={{ padding: 0 }}>
 							<div>
-								<span style={{ fontSize: "30px", fontWeight: "bold" }}>diSlack</span>
+								<span style={{ fontSize: "45px", fontWeight: "bold" }}>diSlack</span>
 
-								<span style={{ fontSize: "20px", fontWeight: "bold", marginLeft: "3%" }}>
+								<span style={{ fontSize: "30px", fontWeight: "bold", marginLeft: "3%" }}>
 									Team Crong - Team Leader
 								</span>
 							</div>
 							<div style={{ marginTop: 10 }}>
-								<FiCalendar size={15} /> <span style={{ fontSize: 16 }}>01/2019 ~ 01/2019</span>
+								<FiCalendar size={20} /> <span style={{ fontSize: 20 }}>01/2019 ~ 01/2019</span>
+							</div>
+							<div style={{ marginLeft: -2, marginTop: 10 }}>
+								<TiSpannerOutline size={23} /> <span style={{ fontSize: 20 }}>Front-End</span>
+							</div>
+							<div style={{ marginTop: 10 }}>
+								<GoNote size={23} /> <span style={{ fontSize: 20 }}>slack cloning</span>
+							</div>
+							<div style={{ marginTop: 10 }}>
+								<FaGithub size={23} />
+								<span
+									className="webSite"
+									style={{ fontSize: 20 }}
+									onClick={() => {
+										window.open("https://github.com/LEEJAEHYUB/diSlack_client");
+									}}
+								>
+									https://github.com/LEEJAEHYUB/diSlack_client
+								</span>
 							</div>
 						</Col>
 					</Row>
